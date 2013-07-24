c8be3a3f960548c05eb283327ad35d232d2ba857
Faker Skandrani <fskandrani@sierrawireless.com>
2013-07-24 14:17:25 +0200

    handle the case where there is no system

--- app/scripts/controllers/token.js
+++ app/scripts/controllers/token.js
@@ -5,9 +5,11 @@ appmodule.controller('TokenCtrl',
             $scope.pageTitle = "token recovery";
             $cookies.avop_access_token = $routeParams.access_token;
             ApplicationService.getSystems(function(result) {
-                config.systemUid = result[0].uid;
-                config.applicationUid = result[0].applications[0].uid;
-                config.applicationName = result[0].applications[0].name;
+                if (result.length) {
+                    config.systemUid = result[0].uid;
+                    config.applicationUid = result[0].applications[0].uid;
+                    config.applicationName = result[0].applications[0].name;
+                }
                 $location.path('/home');
             });
         });
